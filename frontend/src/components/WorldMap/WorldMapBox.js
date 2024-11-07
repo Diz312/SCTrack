e29c7cff5ec4209a9e970e9e7488eb348fdedd01
Diz312 <34927857+Diz312@users.noreply.github.com>
2024-11-06 21:02:05 -0600

    Flask serving the access token finally  - SPELLING !!!

--- frontend/src/components/WorldMap/WorldMapBox.js
+++ frontend/src/components/WorldMap/WorldMapBox.js
@@ -1,23 +1,45 @@
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
 // import 'mapbox-gl/dist/mapbox-gl.css';
 import './WorldMapBox.css'
 
 function WorldMapBox() {
-
-  const mapRef = useRef()
-  const mapContainerRef = useRef()
+  const [MAPBOX_ACCESS_TOKEN, setMapBoxAccessToken] = useState('');
+  const mapRef = useRef();
+  const mapContainerRef = useRef();
 
   useEffect(() => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiZGl6MzEyIiwiYSI6ImNtMzRvYTJraTAwZ28ycnB2ZmJpYWhoOHAifQ.76sBUjcSBy9nOGxB6i45_w'
+    // Fetch the Mapbox key from the Flask API
+    fetch('http://127.0.0.1:5000/api/config')
+      .then(response => response.json())
+      .then(data => {
+        console.log('Fetched data:', data); // Log the fetched data
+        if (data.MAPBOX_ACCESS_TOKEN) {
+          setMapBoxAccessToken(data.MAPBOX_ACCESS_TOKEN);
+        } else {
+          console.error('Mapbox key is missing in the response');
+        }
+      })
+      .catch(error => console.error('Error fetching Mapbox key:', error));
+  }, []);
+
+  useEffect(() => {
+    console.log('Mapbox key in useEffect:', MAPBOX_ACCESS_TOKEN); // Debugging log
+    if (!MAPBOX_ACCESS_TOKEN) {
+      console.log('Mapbox key is not available');
+      return; // Ensure map is initialized only when MAP_BOX_ACCESS_TOKEN is available
+    }
+
+    mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN; // Use the fetched MAP_BOX_ACCESS_TOKEN
+
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
     });
 
     return () => {
-      mapRef.current.remove()
-    }
-  }, [])
+      mapRef.current.remove();
+    };
+  }, [MAPBOX_ACCESS_TOKEN]); // Ensure this useEffect depends on map_box_access_token
 
   return (
     <>
