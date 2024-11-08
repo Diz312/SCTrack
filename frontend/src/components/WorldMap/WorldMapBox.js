a0201932620710af52ceab7ddd70642340d767e8
Diz312 <34927857+Diz312@users.noreply.github.com>
2024-11-08 17:42:52 -0600

    working sidebar and button

--- frontend/src/components/WorldMap/WorldMapBox.js
+++ frontend/src/components/WorldMap/WorldMapBox.js
@@ -1,25 +1,37 @@
 import { useRef, useEffect, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
-// import 'mapbox-gl/dist/mapbox-gl.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import './WorldMapBox.css'
 
-const INITIAL_CENTER = [-98.5795, 39.8283] //TODO: get from config
-const INITIAL_ZOOM = 3 //TODO: get from config  
-
 function WorldMapBox() {
-  const [MAPBOX_ACCESS_TOKEN, setMapBoxAccessToken] = useState('');
+  //TODO: get from config
+  const initCenter = [-98.5795, 39.8283];
+  const initZoom = 3;
+  
+  //set states
+  const [center, setCenter] = useState([-98.5795, 39.8283]) //TODO: get from config
+  const [zoom, setZoom] = useState(3) //TODO: get from config  
+  const [mapboxAccessToken, setMapboxAccessToken] = useState('');
+
+  //set refs
   const mapRef = useRef();
   const mapContainerRef = useRef();
-  const [center, setCenter] = useState(INITIAL_CENTER)
-  const [zoom, setZoom] = useState(INITIAL_ZOOM)
-
+ 
+  //set functions
+  const handleReset = () => {
+    mapRef.current.flyTo({
+      center: initCenter,
+      zoom: initZoom,
+    });
+  };
+  
   useEffect(() => { // Fetch the Mapbox key from the Flask API. The Flask route will run config_loader.py to pull key from .env that needs to be in root directory
     fetch('http://127.0.0.1:5000/api/config')
       .then(response => response.json())
       .then(data => {
         console.log('Fetched data:', data); // Log the fetched data
         if (data.MAPBOX_ACCESS_TOKEN) {
-          setMapBoxAccessToken(data.MAPBOX_ACCESS_TOKEN);
+          setMapboxAccessToken(data.MAPBOX_ACCESS_TOKEN);
         } else {
           console.error('Mapbox key is missing in the response');
         }
@@ -27,37 +39,41 @@ function WorldMapBox() {
       .catch(error => console.error('Error fetching Mapbox key:', error));
   }, []);
 
-  useEffect(() => { // Initialize the map and fly to the initial center and zoom
-    if (!MAPBOX_ACCESS_TOKEN) return;
+  useEffect(() => { // Initialize the map
+    if (!mapboxAccessToken) return;
 
-    mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
+    mapboxgl.accessToken = mapboxAccessToken;
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
-    });
-
-    mapRef.current.on('load', () => {
-      mapRef.current.flyTo({
-        center: INITIAL_CENTER,
-        zoom: INITIAL_ZOOM,
-        speed: 0.5,
-        curve: 1,
-      });
     });
 
     return () => {
       mapRef.current.remove();
     };
-  }, [MAPBOX_ACCESS_TOKEN]); // Only depend on MAPBOX_ACCESS_TOKEN
+  }, [mapboxAccessToken]); // Only depend on MAPBOX_ACCESS_TOKEN
 
-  useEffect(() => { // When user pans the map, update center and zoom info so it can be used in the sidebar
+  useEffect(() => { // Do the initial fly to center and zoom
+    if (!mapRef.current) return;
+
+    mapRef.current.on('load', () => {
+      mapRef.current.flyTo({
+        center: [-98.5795, 39.8283],
+        zoom: 3,
+        speed: 0.5,
+        curve: 1,
+      });
+    });
+  }, [mapRef]); // Depend on center and zoom
+
+  useEffect(() => { // LISTENER: When user pans the map, update center and zoom info so it can be used in the sidebar
     if (!mapRef.current) return;
 
     const handleMove = () => {
-      const mapCenter = mapRef.current.getCenter();
-      const mapZoom = mapRef.current.getZoom();
-      // console.log('Map moved:', mapCenter, mapZoom); // Debugging: log the new center and zoom
-      setCenter([mapCenter.lng, mapCenter.lat]);
-      setZoom(mapZoom);
+    const mapCenter = mapRef.current.getCenter();
+    const mapZoom = mapRef.current.getZoom();
+    console.log('Map moved:', mapCenter, mapZoom); // Debugging: log the new center and zoom
+    setCenter([mapCenter.lng, mapCenter.lat]);
+    setZoom(mapZoom);
     };
 
     mapRef.current.on('move', handleMove);
@@ -65,14 +81,15 @@ function WorldMapBox() {
     return () => {
       mapRef.current.off('move', handleMove);
     };
-  }, [mapRef]); // Correct dependency to mapRef
-  
+  }, []); // Correct dependency to mapRef
+
   return (
     <>
-      <div className="sidebar">
-        Longitude: {center[0].toFixed(4)} | Zoom: {zoom.toFixed(2)}
-      </div>
       <div id='map-container' ref={mapContainerRef} />
+      <div className="sidebar">
+        Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} | Zoom: {zoom.toFixed(2)}
+      </div>
+      <button className="reset-button" onClick={handleReset}>Reset</button>
     </>
   )
 }
